68a442d0ca36393ee0cc7c1a39363919c9645db8
César De la Vega <cdsar626@gmail.com>
2019-03-14 16:26:40 -0400

    Fixed port and host

--- routes/Hello.js
+++ routes/Hello.js
@@ -1,38 +1,38 @@
 const opts = {
-	// method
-	// url || path
-	schema: {
-		/*
-		body: {
-		    type: 'object',
-		    properties: {
-		    	name: { type: 'string' }
-		    },
-		    required: ['name']
-		}
-		*/
-		/*
-		querystring: {
-			name: { type: 'string' },
-			age: { type: 'integer' },
-		},
-		*/
-		// params (work as querystring I guess)
-		response: {
-			200: {
-				type: 'object',
-				properties: {
-					hello: { type: 'string' },
-				},
-			},
-		},
-	},
-	// handler aka function (req, reply)
-	// bodyLimit - defaults to 1048576 (1MiB)
-	// logLevel
+  // method
+  // url || path
+  schema: {
+    /*
+    body: {
+        type: 'object',
+        properties: {
+          name: { type: 'string' }
+        },
+        required: ['name']
+    }
+    */
+    /*
+    querystring: {
+      name: { type: 'string' },
+      age: { type: 'integer' },
+    },
+    */
+    // params (work as querystring I guess)
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          hello: { type: 'string' },
+        },
+      },
+    },
+  },
+  // handler aka function (req, reply)
+  // bodyLimit - defaults to 1048576 (1MiB)
+  // logLevel
 };
 
 fastify.get('/Hello', opts, async (req, reply) => {
-	console.log(req.req);
-	return { hello: 'Hola' }
+  console.log(req.req);
+  return { hello: 'Hola' }
 })
--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const routesLoader = require('fastify-loader'); // load all routes from director
 const compress = require('fastify-compress'); // compress responses
 const helmet = require('fastify-helmet'); // Important security headers
 const fastify = require('fastify')({
-	logger: {
-		level: 'info',
-		file: 'logs.txt',
-	},
+  logger: {
+    level: 'info',
+    //file: 'logs.txt',
+  },
 });
 
 fastify.register(helmet);
@@ -16,12 +16,15 @@ fastify.register(routesLoader, { paths: ['./routes/*.js'] });
 fastify.register(static, { root: path.join(__dirname, 'dist') });
 
 const start = async () => {
-	try {
-		await fastify.listen(5000);
-	} catch(err) {
-		fastify.log.error(err);
-		process.exit(1);
-	}
+  let port = process.env.PORT || 5000;
+    let host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
+  try {
+    await fastify.listen(port, host);
+    console.log(`Listening in ${host}:${port}`);
+  } catch(err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
 }
 
 start();
